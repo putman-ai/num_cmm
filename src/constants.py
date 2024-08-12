a14c3c5a549741000d9a3474e5547dfa5c613b16
John E. Putman II <157129616+putman-ai@users.noreply.github.com>
2024-08-12 15:55:51 -0700

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+# constants
+from pathlib import Path
+
+FIGSIZE = (6, 4)
+DATA_DIR = Path('c:/Users/John/Downloads/')
+CRYPTO_OUTPUT_DIR = Path('num_cmm/db/crypto_mm/')
+RATINGS_OUTPUT_DIR = Path('num_cmm/db/ratings/')
